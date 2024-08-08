a3c6d6dff9144da8344210b5085800094e1ecd99
Antonyxit <carlosmorga45@gmail.com>
2024-08-07 18:22:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const rl = createInterface(process.stdin, process.stdout)
 //const app = express()
 //const port = process.env.PORT || 8080;
 
-say('Stich\nBot', {
+say('Nati-Fati\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
