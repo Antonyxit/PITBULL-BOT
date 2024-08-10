73a3729ab8c517bac5e823de02fd2dba6b5796ba
Antonyxit <carlosmorga45@gmail.com>
2024-08-09 23:28:47 -0600

    Update menu-porno.js

--- plugins/menu-porno.js
+++ plugins/menu-porno.js
@@ -122,7 +122,7 @@ console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗
 console.log(e)}}
 
 //handler.command = /^(menu|menú|memu|memú|help|info|comandos|2help|menu1.2|ayuda|commands|commandos|menucompleto|allmenu|allm|m|\?)$/i
-handler.command = /^(ManjiroPajero|ManjiroPornero|\?)$/i
+handler.command = /^(Hornymenu|Menuhorny|\?)$/i
 export default handler
 
 const more = String.fromCharCode(8206)
